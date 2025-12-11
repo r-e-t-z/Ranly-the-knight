1c703b9929216360cd6dcf124080a09c44749cd5
777Riki777 <bezborodko.viktor@yandex.ru>
2025-12-11 05:46:03 +0300

    Багфикс, уведомления о новом предмете

---
+++ Assets/Game/Scripts/Dialogues/AnimatorAutoDisable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class AnimationAutoDisable : MonoBehaviour
+{
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnAnimationFinished()
+    {
+        if (animator != null)
+        {
+            animator.enabled = false; 
+        }
+    }
+}
